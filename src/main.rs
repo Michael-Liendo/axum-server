53b0c51ae2118fa8918736677488d124d5a32c3c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-03 19:07:06 -0400

    feat: first route and first tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("->> LISTENING on http://{addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
 }
