17f0520be08ad95d29e59db6c4dbff211d31c97e
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-04 20:25:34 -0400

    feat: handle hello

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::net::SocketAddr;
 
-use axum::response::Html;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handle_hello));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("->> LISTENING on http://{addr}\n");
@@ -17,4 +14,10 @@ async fn main() {
         .serve(routes_hello.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handle_hello() -> impl IntoResponse {
+    println!("-> {:<12} - handler_hello", "HANDLER");
+
+    Html("Hello <strong>World!!!</strong>")
 }
