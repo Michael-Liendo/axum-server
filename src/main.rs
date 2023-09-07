6c6e3f0aea0b734b8533f9286b9323f848a87c98
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-07 17:57:29 -0400

    feat: simple login example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use axum::extract::{Path, Query};
 use serde::Deserialize;
 use std::net::SocketAddr;
+use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
-use axum::response::{Html, IntoResponse};
+pub use self::error::{Error, Result};
+
+use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::Router;
+use axum::{middleware, Router};
+
+mod error;
+mod web;
 
 #[tokio::main]
 async fn main() {
     let routes_all = Router::new()
         .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -19,6 +28,14 @@ async fn main() {
         .serve(routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    println!();
+
+    res
 }
 
 fn routes_static() -> Router {
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,10 +1,15 @@
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
 
     hc.do_get("/hello2/michael").await?.print().await?;
+
+    let req_login = hc.do_post("/api/login", json!({"username":"demo1", "pwd":"welcome"}));
+
+    req_login.await?.print().await?;
 
     // hc.do_get("/src/main.rs").await?.print().await?;
 
