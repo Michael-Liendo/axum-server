fd2f8f0b5cd7ea062ed03e5e8aa27211397cf54d
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-07 17:08:32 -0400

    feat: url params and static routes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
-use axum::extract::Query;
+use axum::extract::{Path, Query};
 use serde::Deserialize;
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
 
 use axum::response::{Html, IntoResponse};
-use axum::routing::get;
+use axum::routing::{get, get_service};
 use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handle_hello));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("->> LISTENING on http://{addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handle_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
 #[derive(Debug, Deserialize)]
@@ -23,10 +36,18 @@ struct HelloParams {
     name: Option<String>,
 }
 
+// /hello?name=Michael
 async fn handle_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("-> {:<12} - handler_hello", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello <strong>{name}!!!</strong>"))
+}
+
+// /hello2/michael
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("-> {:<12} - handler_hello", "HANDLER");
 
     Html(format!("Hello <strong>{name}!!!</strong>"))
 }
