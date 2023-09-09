a2ced0de6373766e0a91de2ecc4df7732e476987
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-08 20:11:52 -0400

    feat: errors

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 #[derive(Debug)]
 pub enum Error {
     LoginFail,
+    TicketDeleteFailIDNotFound { id: u64 },
 }
 
 impl IntoResponse for Error {
